65b8417973e2b5cb90458e676eb96ba167782380
Domitrius Clark <domitriusaclark@gmail.com>
2022-05-20 14:46:26 -0400

    Styled the contact card after QR scan.

--- app/routes/scanner.jsx
+++ app/routes/scanner.jsx
@@ -5,7 +5,6 @@ import { db } from "~/utils/db.server";
 import { Octokit } from "@octokit/core";
 
 import Layout from "~/components/Layout";
-import Card from '~/components/Card';
 
 export const action = async ({ request }) => {
   const form = await request.formData();
@@ -51,8 +50,6 @@ export default function ScannerRoute() {
   const transition = useTransition();
   const [data, setData] = React.useState();
 
-  console.log(contact)
-
   const isSaving =
     transition.state === "submitting" &&
     transition.submission.formData.get("_action") === "save";
@@ -63,13 +60,15 @@ export default function ScannerRoute() {
     }
   }, [isSaving]);
 
+  console.log(data)
+
   return (
     <Layout>
       <div className="flex flex-col items-center">
-        <h1 className="text-xl">Scanner</h1>
+        <h1 className="text-xl mb-10">Scanner</h1>
         {data ? (
           <>
-            <Form method="POST" replace>
+            <Form method="POST" className="flex flex-col space-y-3 w-72 items-center justify-center space-around border-2 border-sky-200 rounded-lg p-4" replace>
               <input type="hidden" name="firstName" value={data.firstName} />
               <input type="hidden" name="lastName" value={data.lastName} />
               <input
@@ -78,15 +77,22 @@ export default function ScannerRoute() {
                 value={data.githubUsername}
               />
               <input type="hidden" name="shirtSize" value={data.shirtSize} />
-              {contact && <Card person={{ ...data, ...contact }} />}
-              <label>
+              <img src={contact.avatar_url} alt="Github Avatar" className="rounded-full h-16 w-16 border-1 border-2 border-sky-500" />
+              <p>
+                {data.firstName} {data.lastName}
+                <br />
+                Github: {data.githubUsername}
+                <br />
+                Twitter: {data.twitter}
+              </p>
+              <label className="flex flex-col w-full">
                 Notes:
-                <textarea className="border-1 border-black rounded-md" name="notes"></textarea>
+                <textarea className="border-2 border-black rounded-md" name="notes"></textarea>
               </label>
-              <button className="rounded-lg bg-blue-400 text-white p-3 mx-2" type="submit" name="_action" value="save">
+              <button className="rounded-lg w-full bg-blue-400 text-white p-3 mx-2" type="submit" name="_action" value="save">
                 Save
               </button>
-              <button className="rounded-lg bg-yellow-400 p-3" type="button" onClick={() => setData(null)}>
+              <button className="rounded-lg w-full bg-yellow-400 p-3" type="button" onClick={() => setData(null)}>
                 Scan again
               </button>
             </Form>
@@ -117,6 +123,6 @@ export default function ScannerRoute() {
           />
         )}
       </div>
-    </Layout>
+    </Layout >
   );
 };
