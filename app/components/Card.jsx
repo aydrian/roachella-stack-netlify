ac95a8286a565baf90217e93ea64815f674d29a2
Domitrius Clark <domitriusaclark@gmail.com>
2022-05-22 14:53:50 -0400

    finalized adding base card styles around contacts and cards. added delete functionality for contacts and individual cards.

--- app/components/Card.jsx
+++ app/components/Card.jsx
@@ -1,17 +1,27 @@
-export default function Card({ person }) {
+export default function Card({ details, canDelete = false }) {
   return (
     <div className="flex flex-col space-y-3 w-30 items-center justify-center space-around border-2 border-sky-200 rounded-lg p-4">
-      <img src={person.avatar_url} alt="Github Avatar" className="rounded-full h-16 w-16 border-1 border-2 border-sky-500" />
+      <img src={details.avatar_url} alt="Github Avatar" className="rounded-full h-16 w-16 border-1 border-2 border-sky-500" />
       <p>
-        {person.firstName} {person.lastName}
+        {details.firstName} {details.lastName}
         <br />
-        GitHub: {person.githubUsername}
+        GitHub: {details.githubUsername}
         <br />
-        Twitter: {person.twitter}
+        Twitter: {details.twitter}
         <br />
-        T-Shirt Size: {person.shirtSize}
+        T-Shirt Size: {details.shirtSize}
       </p>
-      <img src={person.qrCode} alt="qr code" />
+      {details.qrCode ? <img src={details.qrCode} alt="qr code" /> : null}
+      {
+        canDelete
+          ? (
+            <>
+              <input type="hidden" name="_method" value="delete" />
+              <button type="submit" className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded-full">Delete</button>
+            </>
+          )
+          : null
+      }
     </div>
   )
 }
